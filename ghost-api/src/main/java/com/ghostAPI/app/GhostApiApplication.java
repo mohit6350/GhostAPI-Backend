ea937ebfc21bc4daafdd505354b1f9a37eb1d416
Mohit khare <35894819+mohit6350@users.noreply.github.com>
2024-02-02 01:20:17 +0530

    initial setup

---
+++ ghost-api/src/main/java/com/ghostAPI/app/GhostApiApplication.java
@@ -0,0 +1,13 @@
+package com.ghostAPI.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostApiApplication.class, args);
+	}
+
+}
---
+++ ghost-api/src/test/java/com/ghostAPI/app/GhostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostAPI.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
